b7a2b815e1dbe0627d20f752c8cb80e8562e98af
Kyle <kyledwalker95@gmail.com>
2024-06-12 16:20:01 -0400

    origin

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -43,6 +43,7 @@ const deleteWorkout = async (req, res) => {
         return res.status(404).json({"message": "No such workout"})
     }
 
+
 const workout = await Workout.findOneAndDelete({_id: id})
     if (!workout){
         return res.status(404).json({error: "No such workout"})
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,20 +1,22 @@
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
 
 // Components
 import WorkoutDetails from '../components/WorkoutDetails'
+import WorkoutForm from "../components/WorkoutForm"
 
 
 const Home = () => {
-    const [workouts, setWorkouts] = useState(null)
+    const {workouts, dispatch} = useWorkoutsContext()
 
     useEffect(() => {
-    const fetchWorkouts = async () => {
-        const response = await fetch('/api/workouts')
-        const json = await response.json()
+        const fetchWorkouts = async () => {
+            const response = await fetch('/api/workouts')
+            const json = await response.json()
 
-        if (response.ok) {
-            setWorkouts(json)
-        }
+            if (response.ok) {
+                dispatch({type: 'SET_WORKOUTS', payload: json})
+            }
     }
 
     fetchWorkouts()
@@ -27,8 +29,10 @@ const Home = () => {
                     <WorkoutDetails key={workout._id} workout = {workout}/>
                 ))}
             </div>
+            <WorkoutForm></WorkoutForm>
         </div>
     )
+
 }
 
 export default Home
